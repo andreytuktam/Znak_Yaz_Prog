da992b83f4765fe662317a2da701aa7f812792b4
Andrey Tuktam <tuktam@mail.ru>
2022-06-25 07:28:29 +0700

    Prog_6

---
+++ Prog_6/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 3;
+int b = 7;
+int c = 1;
+int d = 3;
+int e = 4;
+int f = 5;
+int max = 0;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+if (f > max) max = f;
+Console.Write("max= ");
+Console.WriteLine(max);
+
