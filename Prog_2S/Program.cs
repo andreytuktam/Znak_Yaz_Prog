ce33125b71b15b1f2739b1037f7f74716bb232f3
Andrey Tuktam <tuktam@mail.ru>
2022-06-27 20:27:27 +0700

    Prog_3S

---
+++ Prog_2S/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB == numberA * numberA){
+    Console.WriteLine("B = A * A");
+} else{
+    Console.WriteLine("B != A * A");
+}
+   
